85f73036fa4db55a70143f78db79f0b342f03da1
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-20 16:26:07 +0530

    Update EmployeePayrollAppApplication.java

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -14,5 +14,5 @@ public class EmployeePayrollAppApplication {
 			ApplicationContext context=SpringApplication.run(EmployeePayrollAppApplication.class, args);
 			log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 			log.info("Employee Payroll DB User is {}",context.getEnvironment().getProperty("spring.datasource.username"));
-		}
+		} 
 }
